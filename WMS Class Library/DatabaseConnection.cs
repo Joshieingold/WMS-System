27262ef810870bc9aa4558977b04668aff4fff0d
Josh <JoshLynch2001@gmail.com>
2025-07-08 22:23:29 -0300

    Add project files.

---
+++ WMS Class Library/DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+namespace WMS_Class_Library
+{
+
+    public class DatabaseConnection
+    {
+        string connectionString = "Host=localhost;Username=postgres;Password=;Database=postgres";
+    
+    public void ReadAllData(string databaseName)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand($"SELECT * FROM {databaseName}", conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string columnName = reader.GetName(i);
+                    object value = reader.GetValue(i);
+                    Console.Write($"{columnName}: {value}  ");
+                }
+                Console.WriteLine(); // New line after each row
+            }
+        }
+
+    } 
+}
---
+++ WMS System/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using WMS_Class_Library;
+
+namespace WMS_System;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    
+
+    DatabaseConnection databaseConnection = new DatabaseConnection();
+    public MainWindow()
+    {
+        InitializeComponent();
+        // AdminWindow admin = new AdminWindow();
+        // admin.Show();
+    }
+    private void Button_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            databaseConnection.ReadAllData($"{query_database_textbox.Text}");
+        }
+        catch
+        {
+            Console.WriteLine("Empty Table Selected");
+        }
+    }
+
+}
